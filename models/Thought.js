7488c02a0b28094405684963210a685c4cd6d829
ParkerCF <110678158+ParkerCF@users.noreply.github.com>
2023-03-18 18:04:40 -0400

    added Thought model

---
+++ models/Thought.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const reactionSchema = require("./Reaction");
+
+const thoughtSchema = new mongoose.Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      get: (createdAt) => createdAt.toDateString(),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = mongoose.model("thought", thoughtSchema);
+
+module.exports = Thought;
